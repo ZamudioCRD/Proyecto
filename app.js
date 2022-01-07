d3a48096cb1c613adab64730db45fdee91b93ca7
ZamudioCRD <94937100+ZamudioCRD@users.noreply.github.com>
2022-01-06 23:56:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const init = () => {
+  alert('Bienvenido! Selecciona un Pokémon para obtener su información.');
   
-    fetch('https://pokeapi.co/api/v2/pokemon')
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151')
       .then((respuesta) => {
         return respuesta.json();
       })
@@ -11,19 +12,29 @@ const init = () => {
         const section = document.querySelector('section');
         let info = '';
         let contadorParaImagen = 0;
-  
-        for (let i = 0; i < personajes.length; i++) {
+
+        for (let i=0; i < personajes.length; i++) {
+
             contadorParaImagen + i;
             info += `
-              <div class="tarjeta">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png" height="200" width="200">
+              <div class="tarjeta" onclick="fetchPokemonData('${personajes[i].url}')">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png" height="250" width="250">
                 <p class="name">${personajes[i].name}</p>
-                <a href="${personajes[i].url}">Mas info.</a>
               </div>
             `
-        }   
+          }   
         return section.innerHTML = info;
       })
   }
   
   document.onload = init();
+
+    function fetchPokemonData(pokemon){
+      let url = pokemon
+      fetch(url)
+      .then(response => response.json())
+      .then(function(pokeData){
+        console.log(pokeData)
+        alert('ID del Pokémon: ' + pokeData.id +'\n'+ 'Nombre: ' + pokeData.name +'\n\n'+ 'Experiencia Base: ' + pokeData.base_experience + ' PX' +'\n'+ 'Peso: ' + pokeData.weight +' Lb' +'\n'+ 'Altura: ' + pokeData.height + ' ft')
+      })
+    }
